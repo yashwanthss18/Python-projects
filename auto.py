02ef2f74e4b7e5b1f2ee363be982013e8f2d220b
yashwanthss18 <yashwanth@yashwanths-MacBook-Air.local>
2024-09-08 22:20:24 +0530

    initialcommit

---
+++ auto.py
@@ -0,0 +1,32 @@
+# python3 -m pip install openpyxl 
+#use this to install the openpyxl package in python
+
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference  #from chart module two classes BarChart and Reference are imported
+
+def process_workbook(filename):
+    wb = xl.load_workbook(filename) # storing in workbook object
+    sheet =wb['Sheet1']
+
+    for row in range(2,sheet.max_row+1):    #to include the last row too
+        cell = sheet.cell(row,3)
+        new_price = cell.value*0.9 # reducing the price value by 10%
+        new_price_cell = sheet.cell(row,4)
+        new_price_cell.value=new_price
+
+    #Values will store the information in the 4th column
+    values =Reference(sheet,
+            min_row=2,
+            max_row=sheet.max_row,
+            min_col=4,
+            max_col=4)
+
+    chart = BarChart()  #creating a object
+    chart.add_data(values)  #passing the values
+    sheet.add_chart(chart,'f2')
+
+
+    wb.save(filename)
+
+filename =input("enter file name:")
+process_workbook(filename)
